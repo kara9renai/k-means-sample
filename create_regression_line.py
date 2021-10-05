ea72ce23c0b33e67e24e156a41a363a2e9a3d4e0
ryota <r.enumura@icloud.com>
2021-10-05 10:54:41 +0900

    add new file

---
+++ create_regression_line.py
@@ -0,0 +1,23 @@
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+# csvファイルは任意のものを選ぶ
+df = pd.read_csv("csv/Group_B/ggap_tfp_sgp.csv")
+
+x = df[["ggap"]]
+y = df[["tfp"]]
+
+model_lr = LinearRegression()
+model_lr.fit(x,y)
+
+plt.plot(x, y, 'o')
+plt.plot(x, model_lr.predict(x), linestyle="solid")
+plt.title("Regression Analysis in Singapore")
+plt.show()
+
+print('モデル関数の回帰変数 w1: %.3f' %model_lr.coef_)
+print('モデル関数の切片 w2: %.3f' %model_lr.intercept_)
+print('y= %.3fx + %.3f' % (model_lr.coef_ , model_lr.intercept_))
+print('決定係数 R^2： ', model_lr.score(x, y))
